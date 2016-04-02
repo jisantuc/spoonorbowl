ef4e95d1bac466673fa4fd448373eb68790ad9a8
james <james.santucci@gmail.com>
2016-04-02 14:12:20 -0400

    added more images to images folder, modified dbsetup and second template page so that last image score is shown and average score for that image is shown

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
 import random
 import os
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
+from dbsetup import Session, Vote
 
 app = Flask(__name__)
+
+def get_random_image():
+    images = os.listdir('static/images')
+    return random.choice(images)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    images = os.listdir('static/images')
-    image = random.choice(images)
+    image = get_random_image()
     return render_template(
         'index.html',
         image=image
     )
 
-@app.route('/process', methods=['POST', 'GET'])
+@app.route('/process', methods=['POST'])
 def process():
     ''' records votes and redirects to another image '''
+
     formData = request.values
-    print formData
-    return index()
+    vals = formData.to_dict()
+
+    im, score = [(k, v) for k, v in vals.items()][0]
+    added = Vote.add(im, score)
+
+    image = get_random_image()
+
+    return render_template(
+        'index_sub.html',
+        image=image,
+        last_image=im,
+        last_score=score,
+        mean_score=added.mean(im)
+    )
 
 if __name__=="__main__":
-	app.run()
+	app.run(debug=True)
