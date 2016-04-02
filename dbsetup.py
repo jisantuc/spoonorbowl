a094e146d3852ef1488368e7b4a6fa0ebc8e57a6
james <james.santucci@gmail.com>
2016-04-02 14:12:20 -0400

    added more images to images folder, modified dbsetup and second template page so that last image score is shown and average score for that image is shown

--- dbsetup.py
+++ dbsetup.py
@@ -1,11 +1,15 @@
 import re
 from sqlalchemy import (
-    create_engine, Column, Integer, String, DateTime, Boolean
+    create_engine, Column, Integer, String, DateTime, Boolean,
+    func
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
+
+engine = create_engine('sqlite:///votes.db')
+Session = sessionmaker(bind=engine)
 
 class Vote(Base):
     __tablename__ = 'vote'
@@ -15,7 +19,7 @@ class Vote(Base):
     value = Column(Integer)
 
     @property
-    def filename(self):
+    def imagename(self):
         return re.sub(
             r'(\.jpg|\.png|\.gif)$',
             '',
@@ -24,13 +28,28 @@ class Vote(Base):
 
     def __repr__(self):
         return (
-            '<image: {image} value: {value}>'.format(
-                image=self.filename, value=self.value
+            '<image: {image}, value: {value}>'.format(
+                image=self.image, value=self.value
             )
         )
 
-engine = create_engine('sqlite:///votes.db')
-Session = sessionmaker(bind=engine)
+
+    def mean(self, image):
+        sess = Session()
+        vote = (sess.query(func.avg(Vote.value))
+                .filter(Vote.image == image)
+                .scalar())
+        return round(vote, 2)
+
+    @classmethod
+    def add(cls, image_file, score):
+        to_add = Vote(image=image_file, value=score)
+        sess = Session()
+        sess.add(to_add)
+        sess.commit()
+        sess.close()
+        return to_add
+
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
